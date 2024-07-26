224f351c49f868e280a98ea5d952c511b1acd7ad
Sofia Vargas <sofia.analie08@gmail.com>
2024-07-26 12:10:27 -0500

    Initial Commit

---
+++ Final Project Group 1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Final Project Group 1
+//
+//  Created by Sofia Vargas on 7/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Final Project Group 1/Final_Project_Group_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Project_Group_1App.swift
+//  Final Project Group 1
+//
+//  Created by Sofia Vargas on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Final_Project_Group_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
